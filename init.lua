a8bf7205c4b62e94fd9417dbf0e5e5d0d76dc00d
ll-hotel <ll-hotel@student.42lyon.fr>
2024-08-04 20:51:08 +0200

    opacity, no autoformat, tabs for indent

--- init.lua
+++ init.lua
@@ -1,2 +1,34 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.g.autoformat = false;
+vim.opt.nu = true;
+vim.opt.relativenumber = true;
+
+vim.opt.tabstop = 4;
+vim.opt.shiftwidth = 4;
+vim.opt.softtabstop = 4;
+vim.opt.smartindent = true;
+vim.opt.expandtab = false;
+
+vim.opt.wrap = false;
+
+vim.opt.hlsearch = false;
+vim.opt.incsearch = true;
+
+vim.opt.termguicolors = true;
+
+vim.opt.scrolloff = 10;
+
+vim.opt.updatetime = 60;
+
+vim.g.user42 = "ll-hotel";
+vim.g.mail42 = "ll-hotel@student.42.fr";
+
+vim.g.mapleader = " ";
+
+vim.keymap.set("n", "<leader>y", '"+y');
+vim.keymap.set("v", "<leader>y", '"+y');
+vim.keymap.set("n", "<leader>Y", '"+Y');
+
+vim.cmd("highlight Normal guibg=none");
